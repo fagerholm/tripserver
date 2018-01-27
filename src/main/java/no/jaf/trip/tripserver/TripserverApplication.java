92b2419b167070006a54917fa01c46727977280f
Jon André Fagerholm <jon.fagerholm@gmail.com>
2018-01-27 10:37:55 +0100

    Initial commit of the application

---
+++ src/main/java/no/jaf/trip/tripserver/TripserverApplication.java
@@ -0,0 +1,32 @@
+package no.jaf.trip.tripserver;
+
+import no.jaf.trip.tripserver.domain.Car;
+import no.jaf.trip.tripserver.integration.CarRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class TripserverApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TripserverApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner init(CarRepository repository) {
+        return args -> {
+            Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+                    "AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+                Car car = new Car();
+                car.setName(name);
+                repository.save(car);
+            });
+            repository.findAll().forEach(System.out::println);
+        };
+    }
+}
---
+++ src/main/java/no/jaf/trip/tripserver/integration/CarRepository.java
@@ -0,0 +1,9 @@
+package no.jaf.trip.tripserver.integration;
+
+import no.jaf.trip.tripserver.domain.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
