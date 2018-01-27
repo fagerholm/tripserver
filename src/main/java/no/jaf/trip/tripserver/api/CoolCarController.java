d417c694ee9d7b57ce4dc9def44e8ace38b6509f
Jon André Fagerholm <jon.fagerholm@gmail.com>
2018-01-27 12:23:06 +0100

    Initial commit of the application

--- src/main/java/no/jaf/trip/tripserver/api/CoolCarController.java
+++ src/main/java/no/jaf/trip/tripserver/api/CoolCarController.java
@@ -2,11 +2,13 @@ package no.jaf.trip.tripserver.api;
 
 import no.jaf.trip.tripserver.domain.Car;
 import no.jaf.trip.tripserver.integration.CarRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.stream.Collectors;
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 class CoolCarController {
     private CarRepository repository;
     public CoolCarController(CarRepository repository) {
--- src/main/java/no/jaf/trip/tripserver/integration/CarRepository.java
+++ src/main/java/no/jaf/trip/tripserver/integration/CarRepository.java
@@ -3,7 +3,9 @@ package no.jaf.trip.tripserver.integration;
 import no.jaf.trip.tripserver.domain.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
 public interface CarRepository extends JpaRepository<Car, Long> {
 }
